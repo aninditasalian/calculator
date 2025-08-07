e3e9b9bf954eff9d68f4d2061e9acbe2dfcdacad
anind <anindita.salian@gmail.com>
2025-08-07 23:15:17 +0530

    added : displaying digits to display, returnResult function, clear and delete buttons

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let num1;
-let operator;
-let num2;
+const OPERATORS = ["+", "-", "*", "/"];
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -35,3 +34,49 @@ function operate(num1, operator, num2) {
         default: return "invalid operator"
     }
 }
+
+let display = document.querySelector(".display");
+display.textContent = "";
+
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+let deleteButton = document.querySelector(".delete");
+
+
+const buttonContainer = document.querySelector(".buttonContainer");
+
+buttonContainer.addEventListener("click", (e) => {
+    if (Number(e.target.className) || (e.target.className == "0") || (OPERATORS.includes(e.target.className))){
+        display.textContent += `${e.target.className}`;
+    }
+    if (e.target.className == "clear") {
+        display.textContent = "";
+    } 
+    if (e.target.className == "delete") {
+        display.textContent = display.textContent.slice(0, -1);
+    }
+})
+
+
+function returnResult() {
+    let expression = display.textContent;
+    
+    let operator;
+    let indexOfOp;
+
+    for (char of expression) {
+        if (OPERATORS.includes(char)) {
+            operator = char;
+            indexOfOp = expression.indexOf(char);
+        }
+    }
+
+    num1 = Number(expression.slice(0, indexOfOp));
+    num2 = Number(expression.slice(indexOfOp + 1));
+
+    let result = operate(num1, operator, num2);
+    display.textContent = result;
+}
+
+
+equal.addEventListener("click", returnResult)

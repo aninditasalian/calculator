6bf49e87c26354bfbde1b74d0d1c12554e1b0ec0
anind <anindita.salian@gmail.com>
2025-08-08 15:02:12 +0530

    enabled decimal input for click events, prevention of multiple decimals

--- script.js
+++ script.js
@@ -41,21 +41,85 @@ display.textContent = "";
 let clear = document.querySelector(".clear");
 let equal = document.querySelector(".equal");
 let deleteButton = document.querySelector(".delete");
-
+let decimal = document.querySelector("#decimal");
 
 const buttonContainer = document.querySelector(".buttonContainer");
 
 buttonContainer.addEventListener("click", (e) => {
-    if (Number(e.target.className) || (e.target.className == "0") || (OPERATORS.includes(e.target.className))){
+    if (!isNaN(e.target.className) || OPERATORS.includes(e.target.className)){
         display.textContent += `${e.target.className}`;
     }
+
+    if (OPERATORS.includes(e.target.className)) {
+        let expression;
+        let operator;
+        expression = display.textContent;
+        operator = expression.slice(-1)
+        expression = expression.slice(0,-1);
+        for (let i = 0; i < expression.length; i++) {
+            if (OPERATORS.includes(expression[i])) {
+                returnResult(expression);
+                display.textContent += operator;
+            }
+        }
+    }
+
+
+    if (e.target.id == "decimal") {
+        display.textContent += `${e.target.className}`;
+        decimal.disabled = true;
+    }
+
     if (e.target.className == "clear") {
         display.textContent = "";
+        decimal.disabled = false;
     } 
+
     if (e.target.className == "delete") {
         display.textContent = display.textContent.slice(0, -1);
+        expression = display.textContent;
+        let indexOfOp;
+        let num;
+        for (let char of expression) {
+            if (OPERATORS.includes(char)) {
+                indexOfOp = expression.indexOf(char);
+                num = expression.slice(indexOfOp + 1);
+            } else {
+                num = display.textContent;
+            }
+            if (!num.includes(".")) {
+                decimal.disabled = false;
+            }
+        }
     }
+    
+    if (e.target.className == "equal") {
+        let expression = display.textContent;
+        returnResult(expression);
+        if (!display.textContent.includes(".")) {
+            decimal.disabled = false;
+        }
+    }
+
 })
+
+function returnResult(expression) {
+
+    let operator;
+    let indexOfOp;
+
+    for (let char of expression) {
+        if (OPERATORS.includes(char)) {
+            operator = char;
+            indexOfOp = expression.indexOf(char);
+            num1 = Number(expression.slice(0, indexOfOp));
+            num2 = Number(expression.slice(indexOfOp + 1));
+            let result = operate(num1, operator, num2);
+            display.textContent = result;
+        }
+    }
+}
+
 
 document.addEventListener("keydown", (e) => {
 
@@ -70,55 +134,12 @@ document.addEventListener("keydown", (e) => {
     } 
 })
 
-
-function returnResult(expression) {
-
-    let operator;
-    let indexOfOp;
-
-    for (char of expression) {
-        if (OPERATORS.includes(char)) {
-            operator = char;
-            indexOfOp = expression.indexOf(char);
-            num1 = Number(expression.slice(0, indexOfOp));
-            num2 = Number(expression.slice(indexOfOp + 1));
-            let result = operate(num1, operator, num2);
-            display.textContent = result;
-        }
-    }
-}
-
-
-equal.addEventListener("click", (e) => {
-    let expression = display.textContent;
-    returnResult(expression)
-});
-
 document.addEventListener("keydown", (e) => {
     let expression = display.textContent;
     if (e.key == "=" || e.key == "Enter") {
         returnResult(expression);
     }
 })
-
-
-let firstExpression = false;
-
-buttonContainer.addEventListener("click", (e) => {
-    let expression;
-    let operator;
-    if (OPERATORS.includes(e.target.className)) {
-        expression = display.textContent;
-        operator = expression.slice(-1)
-        expression = expression.slice(0,-1);
-        for (let i = 0; i < expression.length; i++) {
-            if (OPERATORS.includes(expression[i])) {
-                returnResult(expression);
-                display.textContent += operator;
-            }
-        }
-    }
-}) 
 
 document.addEventListener("keydown", (e) => {
     let expression;

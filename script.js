1633f829372aaad33b49e752b301871f7820bd46
anind <anindita.salian@gmail.com>
2025-08-08 20:29:29 +0530

    fixed keyboard event handling for numbers

--- script.js
+++ script.js
@@ -43,16 +43,16 @@ let equal = document.querySelector(".equal");
 let deleteButton = document.querySelector(".delete");
 let decimal = document.querySelector("#decimal");
 
-let zero = document.querySelector(".zero");
-let one = document.querySelector(".one");
-let two = document.querySelector(".two");
-let three = document.querySelector(".three");
-let four = document.querySelector(".four");
-let five = document.querySelector(".five");
-let six = document.querySelector(".six");
-let seven = document.querySelector(".seven");
-let eight = document.querySelector(".eight");
-let nine = document.querySelector(".nine");
+let zero = document.querySelector("#zero");
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+let nine = document.querySelector("#nine");
 
 numbersArray = [zero, one, two, three, four, five, six, seven, eight, nine];
 
